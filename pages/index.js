7a15b065e1376d9a6f97608b00cd75cc0e65bf1a
Rajib Mondal <mondal021002rajib@gmail.com>
2022-06-23 00:12:03 +0530

    deploying initial website

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
           Lorem Ipsum
         </h2>
         <h1 className="text-5xl md:text-9xl font-bold uppercase tracking-wide">
-          Regime of <br /> Hustler's
+          Regime of <br /> Hustler&apos;s
         </h1>
         <div className="uppercase md:tracking-[6px] bg-gradient-to-r from-purple-900 to-fuchsia-900 py-4 px-8 rounded-full cursor-pointer">
           Underconstruction
